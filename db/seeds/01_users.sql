f2b2cb8ed251a5a565d0d79692f8e0d67ad01d28
Lalika31253 <Lalika31253@gmail.com>
2024-01-27 15:40:35 -0700

    Add project pictures

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds 
+INSERT INTO users (username, email, password) VALUES ('Alice', 'alicejackson@mail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Kira', 'kirasantos@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Alex', 'alexalexon@gmail.com', 'password');
+
+
+-- Admin and User credentials
+INSERT INTO users (username, email, password) VALUES ('admin', 'admin@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('user_1', 'user_1@gmail.com', '1234');
