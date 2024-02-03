5d145d57efc0dc3b17641147bb0996991a3dfabc
Michael Gauthier <mikegauthier222@hotmail.com>
2024-02-03 12:49:35 -0800

    merge

---
+++ db/queries/filter-price.js
@@ -0,0 +1,13 @@
+// const db = require('../connection');
+
+// const filterPrice = () => {
+//   return db.query('SELECT id FROM products WHERE price > 80000 AND price < 190000;;') //put the params here...
+//     .then(data => {
+//       return data.rows;
+//     });
+//   // console.log('hello');
+// };
+
+// console.log(filterPrice());
+
+// module.exports = { filterPrice };
