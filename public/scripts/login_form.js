d798372c37134342e05c06452327656015982698
Lalika31253 <Lalika31253@gmail.com>
2024-02-04 03:14:30 -0700

    Create a POST request to the server for login form

---
+++ public/scripts/login_form.js
@@ -0,0 +1,24 @@
+const { response } = require("express");
+
+$(document).ready(function () {
+  
+  $('.login-form').on('submit', function (event) {
+    event.preventDefault();
+    // Serialize form data
+    const formData = $(this).serialize();
+
+    // Send a POST request to the server
+    $.ajax({
+      method: 'POST',
+      url: '/login',
+      data: formData,
+      success: function (response) {
+        console.log('success');
+      },
+    error: function (error) {
+      console.log(error);
+    }
+    });
+
+  });
+});
