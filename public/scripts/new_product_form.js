2269d792ae693a9f7c9780f29b13499ed8a0e3ae
Lalika31253 <Lalika31253@gmail.com>
2024-02-04 09:05:29 -0700

    Fix merge

--- public/scripts/new_product_form.js
+++ public/scripts/new_product_form.js
@@ -22,12 +22,3 @@ $(document).ready(function () {
 
   });
 });
-<<<<<<< HEAD
-
-  // - That form will make a query post request to the back end
-  // - ie $.post('localhost:8000/api/add')
-  // - backend will have app.post('/api/add')
-
-s
-=======
->>>>>>> bb8c566901ddf6767b462dacd16d3d3f4bd44e3c
