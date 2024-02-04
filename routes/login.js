41ef686b2866613bfcbf1d6669cc6f4d4a02f7ec
Lalika31253 <Lalika31253@gmail.com>
2024-02-04 09:00:13 -0700

    Redirect a user to the main page after authentication

--- routes/login.js
+++ routes/login.js
@@ -5,32 +5,35 @@ const db = require('../db/connection');
 router.route('/')
 .get((req, res) => {
   res.render('login_form');
-})
-// post('/', (req, res) => {
-//   const { username, password } = req.body;
+});
 
-//   const query = `SELECT * FROM users WHERE username = $1`;
-//   console.log(query);
-//   db.query(query, [username])
-//     .then(data => {
-//       const user = data.rows[0];
-//       // console.log(user);
-//       // console.log(req.body);
-//       if (!user) {
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
 
-//         return res.status(401).json({ error: 'Invalid username or password' });
-//       }
+  const query = `SELECT * FROM users WHERE email = $1`;
+  console.log(query);
+  db.query(query, [email])
+    .then(data => {
+      const user = data.rows[0];
+      // console.log(user);
+      // console.log(req.body);
+      if (!user) {
 
-//       if (password !== user.password) {
-//         return res.status(401).json({ error: 'Invalid username or password' });
-//       }
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
 
-//       // req.session.user = user;
-//       res.cookie('user_id', user.id, {maxAge: 24 * 60 * 60 * 1000});
-//       res.json({ message: 'Login successful', user });
-//     })
-//     .catch(err => {
-//       res.status(500).json({ error: err.message });
-//     });
-// });
+      if (password !== user.password) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+
+      // req.session.user = user;
+      res.cookie('user_id', user.id, {maxAge: 24 * 60 * 60 * 1000});
+      // res.json({ message: 'Login successful'});
+      res.redirect('/');
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
 module.exports = router;
