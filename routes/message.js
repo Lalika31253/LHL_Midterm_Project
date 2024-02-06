de277afc01bd339ed0def2041c14e72cb2183590
Lalika31253 <Lalika31253@gmail.com>
2024-02-06 11:08:48 -0700

    add styles

--- routes/message.js
+++ routes/message.js
@@ -10,6 +10,30 @@ const router = express.Router();
 const db = require('../db/connection');
 const { sendMessage } = require('../db/queries/message');
 
+// Middleware to check if the user is logged in and set user object in res.locals
+router.use((req, res, next) => {
+  const userId = req.cookies.user_id;
+  if (userId) {
+    // Fetch user data from the database based on user ID
+    db.query(`SELECT * FROM users WHERE id = $1`, [userId])
+      .then(data => {
+        const user = data.rows[0];
+        if (user) {
+          // If the user exists, set it in res.locals
+          res.locals.user = user;
+          console.log(res.locals.user);
+        }
+        next();
+      })
+      .catch(error => {
+        res.status(500).json({ error: error.message });
+      });
+  } else {
+    // If no user ID in cookies, proceed to next middleware
+    next();
+  }
+});
+
 router.route('/')
 .get((req, res) => {
  console.log('message here',req.query); // For debugging
